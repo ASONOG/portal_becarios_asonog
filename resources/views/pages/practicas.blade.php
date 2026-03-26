738bb4a33afbc433a65a30265dc4d40f5671678c
josemiguel28 <miguel1reyes66@gmail.com>
2026-03-26 12:11:55 -0600

    chore: UI enhances

--- resources/views/pages/practicas.blade.php
+++ resources/views/pages/practicas.blade.php
@@ -38,7 +38,7 @@
                         habilidades y contribuir al bienestar de las comunidades más vulnerables de Honduras.
                     </p>
                 </div>
-                <div data-aos="fade-left" class="bg-zinc-50 rounded-2xl border bg-linear-to-br from-primary-50 to-primary-100 p-8 flex flex-col gap-4">
+                <div data-aos="fade-left" class="bg-zinc-50 rounded-2xl bg-linear-to-br from-primary-50 to-primary-100 p-8 flex flex-col gap-4">
                     <div class="flex items-start gap-4">
                         <div class="w-10 h-10 rounded-lg bg-primary-600 flex items-center justify-center shrink-0">
                             <svg class="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/></svg>
